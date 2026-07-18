5ee6f2c083cdf61dc88b2c1fa4b9214ac9f723d2
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-07-18 16:05:01 +0200

    Fixing file

---
+++ src/Hexer/Core/Options.cs
@@ -0,0 +1,15 @@
+using CommandLine;
+
+// ReSharper disable ClassNeverInstantiated.Global
+
+namespace Hexer.Core
+{
+    public class Options
+    {
+        [Option('n', "noNulls", HelpText = "Write binary without nulls.")]
+        public bool NoNulls { get; set; }
+
+        [Option('i', "input", HelpText = "Set input path.")]
+        public string? Input { get; set; }
+    }
+}
