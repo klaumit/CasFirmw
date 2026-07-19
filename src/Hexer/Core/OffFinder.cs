ced985f211b8b7ce3e8c8c35b3b0cf50b598311a
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-07-19 02:39:50 +0200

    Reading every marker

--- src/Hexer/Core/OffFinder.cs
+++ src/Hexer/Core/OffFinder.cs
@@ -15,6 +15,35 @@ namespace Hexer.Core
             return lines.Read().Read();
         }
 
+        private static IEnumerable<uint> FindBytes(string file, byte[] mask)
+        {
+            var i = 0;
+            uint start = 0;
+            foreach (var line in ReadFile(file))
+            {
+                if (line.Raw == mask[i])
+                {
+                    if (i == 0)
+                        start = line.Off;
+                    i++;
+                    if (i >= mask.Length)
+                    {
+                        yield return start;
+                        i = 0;
+                    }
+                }
+                else
+                {
+                    i = 0;
+                }
+            }
+        }
+
+        private static string ToHex(this IEnumerable<uint> items)
+        {
+            return string.Join(", ", items.Select(i => i.ToString("x8")));
+        }
+
         public static void Run(Options o)
         {
             var inputDir = Path.GetFullPath(o.Input!);
@@ -23,23 +52,21 @@ namespace Hexer.Core
 
             const SearchOption so = SearchOption.AllDirectories;
             var files = Directory.EnumerateFiles(inputDir, "*.xxd", so);
-            foreach (var file in files)
+            foreach (var file in files.Take(1))
             {
                 Console.WriteLine($" * {file}");
 
-                foreach (var line in ReadFile(file).Take(5))
-                {
-                    Console.WriteLine(" " + line + " ");
+                var binIdx = FindBytes(file, "CASIOPVOS200U"u8.ToArray()).ToHex();
+                Console.WriteLine($"    => BIN: {binIdx}");
 
-                    /*
-                    if (string.IsNullOrWhiteSpace(line..Hex))
-                        continue;
-                    if (line.Txt.Contains("PVOSAPL"))
-                    {
-                        Console.WriteLine($" {line}");
-                    }
-                    */
-                }
+                var aplIdx = FindBytes(file, "PVOSAPL"u8.ToArray()).ToHex();
+                Console.WriteLine($"    => APL: {aplIdx}");
+
+                var pvaIdx = FindBytes(file, "PVAPLHEDV20"u8.ToArray()).ToHex();
+                Console.WriteLine($"    => PVA: {pvaIdx}");
+
+                var rldIdx = FindBytes(file, [0x7f, 0x45, 0x4c, 0x46, 0x01, 0x02, 0x01, 0x00]).ToHex();
+                Console.WriteLine($"    => RLD: {rldIdx}");
             }
 
             Console.WriteLine("Done.");
