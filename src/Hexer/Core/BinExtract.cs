eef390e39899ed89caa896b613b00d7b53741ab0
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-08-09 22:28:19 +0200

    Fixing

--- src/Hexer/Core/BinExtract.cs
+++ src/Hexer/Core/BinExtract.cs
@@ -2,6 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using Hexer.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
 
 namespace Hexer.Core
 {
@@ -10,12 +14,17 @@ namespace Hexer.Core
         public static void Run(Options o)
         {
             var inputDir = Path.GetFullPath(o.Input!);
+            var included = Path.GetFullPath(o.Include!);
             var outputFile = Path.GetFullPath(o.Output!);
             Console.WriteLine($" Input => {inputDir}");
+            Console.WriteLine($" Extra => {included}");
             Console.WriteLine($"Output => {outputFile}");
             Console.WriteLine("Extracting binary blobs...");
 
+            var fileSizes = JsonExt.Read<SortedDictionary<int, string>>(included);
             var results = new SortedDictionary<int, SortedSet<string>>();
+
+            Console.WriteLine(JsonConvert.SerializeObject(fileSizes));
 
             JsonExt.Write(outputFile, results);
             Console.WriteLine("Done.");
--- src/Hexer/Tools/JsonExt.cs
+++ src/Hexer/Tools/JsonExt.cs
@@ -6,13 +6,24 @@ namespace Hexer.Tools
 {
     public static class JsonExt
     {
+        public static T? Read<T>(string file)
+        {
+            var json = File.ReadAllText(file, Encoding.UTF8);
+            return JsonConvert.DeserializeObject<T>(json, GetConfig());
+        }
+
         public static void Write(string file, object obj)
         {
-            var json = JsonConvert.SerializeObject(obj, new JsonSerializerSettings
+            var json = JsonConvert.SerializeObject(obj, GetConfig());
+            File.WriteAllText(file, json, Encoding.UTF8);
+        }
+
+        private static JsonSerializerSettings GetConfig()
+        {
+            return new JsonSerializerSettings
             {
                 Formatting = Formatting.Indented
-            });
-            File.WriteAllText(file, json, Encoding.UTF8);
+            };
         }
     }
 }
